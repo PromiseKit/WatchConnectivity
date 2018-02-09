679d90fad23a9f2824f0965bc8f8c4e439c408cb
Max Howell <mxcl@me.com>
2018-02-09 14:55:22 -0500

    Update to PMK6

--- Sources/WCSession+Promise.swift
+++ Sources/WCSession+Promise.swift
@@ -11,11 +11,11 @@ extension WCSession {
 
     /// Sends a message immediately to the paired and active device and optionally handles a response.
     public func sendMessage(_ message: [String: Any]) -> Promise<[String: Any]> {
-        return Promise(.pending) { sendMessage(message, replyHandler: $0.fulfill, errorHandler: $0.reject) }
+        return Promise { sendMessage(message, replyHandler: $0.fulfill, errorHandler: $0.reject) }
     }
 
     /// Sends a data object immediately to the paired and active device and optionally handles a response.
     public func sendMessageData(_ data: Data) -> Promise<Data> {
-        return Promise(.pending) { sendMessageData(data, replyHandler: $0.fulfill, errorHandler: $0.reject) }
+        return Promise { sendMessageData(data, replyHandler: $0.fulfill, errorHandler: $0.reject) }
     }
 }
