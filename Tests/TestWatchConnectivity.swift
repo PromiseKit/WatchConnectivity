b84929f5bbd6419ce9c9fb78a3bb6332016f92f6
Max Howell <mxcl@me.com>
2018-06-20 03:49:43 +0000

    Bump travis platform versions & Cartfile.resolved

--- Tests/TestWatchConnectivity.swift
+++ Tests/TestWatchConnectivity.swift
@@ -20,7 +20,7 @@ class WatchConnectivityTests: XCTestCase {
     func testSuccess() {
 
         let ex = expectation(description: "Success callback")
-        let session = MockSession.default() as! MockSession
+        let session = MockSession.default as! MockSession
         session.fail = false
         session.sendMessage(["message": "test"]).done { response in
             XCTAssertEqual(response as! [String: String], ["response": "Success"])
@@ -39,7 +39,7 @@ class WatchConnectivityTests: XCTestCase {
         }
 
         let ex = expectation(description: "Error callback")
-        let session = MockSession.default() as! MockSession
+        let session = MockSession.default as! MockSession
         session.fail = true
         session.sendMessage(["message": "test"]).done { response in
             XCTFail("Should not succeed")
